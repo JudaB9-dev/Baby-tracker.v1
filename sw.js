82cea95c4ea83808c5c66593e67a86ac95c202a4
JudaB9-dev <yehuda4998@gmail.com>
2026-04-19 09:01:16 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE_NAME = 'baby-tracker-cache-v3';
+const CACHE_NAME = 'baby-tracker-cache-v4';
 const urlsToCache = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
